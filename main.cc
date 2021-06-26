5b7bb3ded278602123ea6c73ebac4ba8b4e90127
Jiahao Li <reg@ljh.me>
2021-06-26 15:49:38 -0400

    Uses 4 spaces for indentation

--- main.cc
+++ main.cc
@@ -4,27 +4,27 @@
 #include <OpenGL/gl.h>
 
 int main(int argc, char *argv[]) {
-  if (!glfwInit()) {
-    return 1;
-  }
+    if (!glfwInit()) {
+        return 1;
+    }
 
-  GLFWwindow *window =
-      glfwCreateWindow(1280, 720, "OpenGL Sandbox", NULL, NULL);
-  if (!window) {
+    GLFWwindow *window =
+        glfwCreateWindow(1280, 720, "OpenGL Sandbox", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
     glfwTerminate();
-    return 1;
-  }
 
-  glfwMakeContextCurrent(window);
-
-  while (!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-
-  glfwTerminate();
-
-  return 0;
+    return 0;
 }
