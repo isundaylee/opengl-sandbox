52d7e018b9f86639eb1c3a85987d3c66aae61df8
Jiahao Li <reg@ljh.me>
2021-06-26 15:31:24 -0400

    Black window working

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ## Requirements
 
 - https://www.glfw.org
+
+## Build steps
+
+```
+cd /path/to/repo
+ln -s build/compile_commands.json .
+mkdir build
+cd build
+cmake ..
+make -j
+```
--- main.cc
+++ main.cc
@@ -1,3 +1,30 @@
-int main(int argc, char* argv[]) {
-    return 0;
+#include <stdio.h>
+
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl.h>
+
+int main(int argc, char *argv[]) {
+  if (!glfwInit()) {
+    return 1;
+  }
+
+  GLFWwindow *window =
+      glfwCreateWindow(1280, 720, "OpenGL Sandbox", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return 0;
 }
